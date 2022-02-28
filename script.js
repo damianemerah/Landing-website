9157b664ad9dfb86d7c5e7f8ac72efddbbebf38a
dammy995 <damianemerah1@gmail.com>
2022-02-28 10:21:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const allSection = document.querySelectorAll("section");
+const checkbox = document.querySelector("input[id='checkbox']");
+const navList = document.querySelector(".nav__list");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+checkbox.addEventListener("click", function (e) {
+  navList.classList.toggle("show-nav");
+});
+
+document.documentElement.addEventListener("click", function (e) {
+  if (checkbox.checked) {
+    if (!e.target.closest(".nav")) {
+      navList.classList.remove("show-nav");
+      checkbox.checked = false;
+    }
+  }
+});
